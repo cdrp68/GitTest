08678ca4ef7a54c91277bc89126e5c688b28b3ec
Cody Rodgers <cdrp68@mail.missouri.edu>
2015-03-17 12:43:05 -0500

    First Commit
    This is the first commit of our Xcode project

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Admin on 3/17/15.
+//  Copyright (c) 2015 Cody Rodgers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
